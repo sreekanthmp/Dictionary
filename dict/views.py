585afe9268b4c7403ee07d07df6424084391f2f1
sreekanthmp <sreekanthmp82@gmail.com>
2022-11-14 13:27:05 +0530

    removed imports

--- dict/views.py
+++ dict/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-import json
 from rest_framework.viewsets import ModelViewSet
-from dict import serializers
-from rest_framework import status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 from dict.serializers import DictSerializer
 from dict.models import Dictionary
-from rest_framework.permissions import IsAuthenticated
 
 class PostViewSet(ModelViewSet):
     queryset = Dictionary.objects.all()
@@ -22,7 +18,6 @@ class PostViewSet(ModelViewSet):
         if(user_list.filter(label=request.query_params.get('search')).exists()):
             exactMatchObject = get_object_or_404(Dictionary,label=request.query_params.get('search'))
             
-            print(exactMatchObject)
             exactMatchObject.search_count = exactMatchObject.search_count + 1
             exactMatchObject.save()
         return Response(serializer.data)  
